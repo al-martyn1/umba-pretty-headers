6a0299b986eb8436e198f4cfd819782e7a0f0cd4
Alexander Martynov <amart@mail.ru>
2021-12-25 21:16:45 +0300

    LLVM initialized

---
+++ src/main/clang.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+
+
+
+
---
+++ src/main/test01.cpp
@@ -0,0 +1,9 @@
+
+#include "clang.h"
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
+
