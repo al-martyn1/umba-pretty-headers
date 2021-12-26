11b46797190769e92ca0a39982203b511f36d654
Alexander Martynov <amart@mail.ru>
2021-12-26 18:49:31 +0300

    Little LLVM/CLang usage/linkage sample completed

--- src/main/clang.h
+++ src/main/clang.h
@@ -1,10 +1,38 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+
+
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
+
+
+#pragma comment(lib, "LLVMBitstreamReader")
+#pragma comment(lib, "LLVMBitReader")
+#pragma comment(lib, "LLVMBitWriter")
+#pragma comment(lib, "LLVMRemarks")
+#pragma comment(lib, "LLVMSupport")
+#pragma comment(lib, "LLVMAnalysis")
+#pragma comment(lib, "LLVMCore")
+#pragma comment(lib, "LLVMFrontendOpenMP")
+#pragma comment(lib, "LLVMDebugInfoDWARF")
+#pragma comment(lib, "LLVMAsmPrinter")
+#pragma comment(lib, "LLVMBinaryFormat")
+
+#pragma comment(lib, "clangSerialization")
+#pragma comment(lib, "clangLex")
+#pragma comment(lib, "clangParse")
+#pragma comment(lib, "clangSema")
+#pragma comment(lib, "clangTidyOpenMPModule")
+#pragma comment(lib, "clangTooling")
+#pragma comment(lib, "clangFrontend")
+#pragma comment(lib, "clangASTMatchers")
+#pragma comment(lib, "clangAST")
+#pragma comment(lib, "clangBasic")
 
 
 
--- src/main/test01.cpp
+++ src/main/test01.cpp
@@ -1,8 +1,66 @@
 
 #include "clang.h"
 
+
+
+#define HAS_ACTION_ARG
+
+
+
+enum ArgNumbers
+{
+    argsMin = 2,
+    appFilenameArgIdx    = 0,
+    inputFilenameArgIdx  = 1,
+
+    #if defined(HAS_ACTION_ARG)
+        inputActionArgIdx     = 2,
+        optionsStartArgIdx    = 3,
+    #else
+        inputActionArgIdx     = 0,
+        optionsStartArgIdx    = 2,
+    #endif
+
+    dummy
+
+
+};
+
+
 int main(int argc, char* argv[])
 {
+    if (argc < argsMin)
+    {
+        //llvm::errs() << "Usage: " << argv[appFilenameArg] << " input.cpp output" << "\n";
+        return -1;
+    }
+
+    std::ifstream inputStream(argv[inputFilenameArgIdx]);
+    if (!inputStream.is_open())
+    {
+        //llvm::errs() << "Cannot open " << argv[1] << "\n";
+        return -1;
+    }
+
+    std::string cxxCode((std::istreambuf_iterator<char>(inputStream)), std::istreambuf_iterator<char>());
+    //llvm::outs() << "File content:\n" << CXXCode << "\n";
+
+    std::vector<std::string> args;
+    for (int arg = optionsStartArgIdx; arg < argc; ++arg)
+    {
+        args.push_back(argv[arg]);
+    }
+
+    if (inputActionArgIdx!=0)
+    {
+        //auto Action = new GenerateKernelsFrontendAction(argv[inputActionArgIdx]);
+        //tooling::runToolOnCodeWithArgs(Action, CXXCode, Args, argv[1]);
+        //delete Action;
+    }
+    
+
+    inputStream.close();
+
     
     return 0;
 }
